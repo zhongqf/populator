99bb99f25f2a77e3b1e9dd7b3f49f97e5c1a903f
zhongqf <zhongqf@gmail.com>
2010-08-17 01:43:41 -0700

    comment out catch_schema_changes for rails3 support.

--- lib/populator/adapters/sqlite.rb
+++ lib/populator/adapters/sqlite.rb
@@ -3,11 +3,11 @@ module Populator
     module Sqlite
       # Executes multiple SQL statements in one query when joined with ";"
       def execute_batch(sql, name = nil)
-        catch_schema_changes do
+        #catch_schema_changes do
           log(sql, name) do
             @connection.transaction { |db| db.execute_batch(sql) }
           end
-        end
+        #end
       end
       
       def populate(table, columns, rows, name = nil)
